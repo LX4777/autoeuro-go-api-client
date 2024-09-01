6463a9144deaf690bd99612d6e391f860c291de1
Vasiliy Rozhkov <profilrv@gmail.com>
2024-09-01 13:51:35 +0500

    fix(responses): getStatuses должен возвращать в status_id тип число, а по факту возвращает строку, поэтому добавляем доп проверку

--- responses/get_statuses_response.go
+++ responses/get_statuses_response.go
@@ -1,10 +1,46 @@
 package responses
 
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+)
+
 type GetStatusesResponse = Response[Status]
 
 type Status struct {
 	Group       string `json:"group"`
-	StatusID    int    `json:"status_id"`
+	StatusID    uint16 `json:"status_id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
+}
+
+func (s *Status) UnmarshalJSON(data []byte) error {
+	type Alias Status
+	aux := &struct {
+		StatusID interface{} `json:"status_id"` // Используем interface{} для обработки разных типов данных
+		*Alias
+	}{
+		Alias: (*Alias)(s),
+	}
+
+	if err := json.Unmarshal(data, aux); err != nil {
+		return err
+	}
+
+	// Обработка поля StatusID
+	switch v := aux.StatusID.(type) {
+	case string:
+		val, err := strconv.ParseUint(v, 10, 16)
+		if err != nil {
+			return fmt.Errorf("failed to convert status_id to uint16: %w", err)
+		}
+		s.StatusID = uint16(val)
+	case float64:
+		s.StatusID = uint16(v)
+	default:
+		return fmt.Errorf("unexpected type for status_id: %T", v)
+	}
+
+	return nil
 }
