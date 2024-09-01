b843472de786cb318fbb8598ed76be7fa96af7fb
Vasiliy Rozhkov <profilrv@gmail.com>
2024-09-01 11:44:23 +0500

    feat(requests_data): добавление get_orders_request_data.go

---
+++ requests_data/get_orders_request_data.go
@@ -0,0 +1,13 @@
+package requests_data
+
+type GetOrdersRequestData struct {
+	Orders  []string      `json:"orders,omitempty"`
+	Filters *OrderFilters `json:"filters,omitempty"`
+}
+
+type OrderFilters struct {
+	From        string  `json:"from"`
+	To          string  `json:"to"`
+	DeliveryKey string  `json:"delivery_key"`
+	PayerKey    *string `json:"payer_key,omitempty"`
+}
