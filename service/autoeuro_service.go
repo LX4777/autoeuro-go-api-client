d914b5e2f656b29aed082ee8e65cb2e95221ad4b
Vasiliy Rozhkov <profilrv@gmail.com>
2024-09-01 12:14:54 +0500

    feat(service): добавление метода  GetDeliveries

--- service/autoeuro_service.go
+++ service/autoeuro_service.go
@@ -13,6 +13,12 @@ func NewAutoeuroService(config client.ApiClientConfig) *AutoeuroService {
 	return &AutoeuroService{client.NewApiClient(config)}
 }
 
+// GetBalance Получение детальной информации о состоянии баланса личного счета
 func (s *AutoeuroService) GetBalance() (*responses.GetBalanceResponse, error) {
 	return client.Request[responses.GetBalanceResponse](s.ApiClient, "/get_balance", nil)
+}
+
+// GetDeliveries Получение массива возможных вариантов получения товара для клиента
+func (s *AutoeuroService) GetDeliveries() (*responses.GetDeliveriesResponse, error) {
+	return client.Request[responses.GetDeliveriesResponse](s.ApiClient, "/get_deliveries", nil)
 }
