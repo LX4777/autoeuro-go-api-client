6e841e2edb83b91f1cf5d9057c80863482048fda
Vasiliy Rozhkov <profilrv@gmail.com>
2024-10-16 16:33:09 +0500

    test: getBalance

---
+++ test/service/service_test.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"github.com/LX4777/autoeuro-go-api-client/client"
+	"github.com/LX4777/autoeuro-go-api-client/client/responses"
+	"github.com/LX4777/autoeuro-go-api-client/service"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestGetBalance(t *testing.T) {
+	mockResponse := responses.GetBalanceResponse{
+		DATA: []responses.Balance{
+			{
+				Balance:      500.0,
+				Credit:       300.0,
+				Ordered:      700.3,
+				Reserved:     400.2,
+				Limit:        1000.0,
+				PayTomorrow:  722.2,
+				ShippingFrom: 23.1,
+				Active:       1,
+			},
+		},
+	}
+
+	ts := NewTestServer("/get_balance", mockResponse)
+	defer ts.Close()
+
+	s := service.NewAutoeuroService(client.ApiClientConfig{
+		BaseURL: ts.Server.URL,
+		Token:   "test-api-key",
+		Timeout: 10 * time.Second,
+	})
+
+	response, err := s.GetBalance()
+	assert.NoError(t, err, "Неожиданная ошибка от GetBalance")
+	assert.NotNil(t, response, "Ответ равен nil")
+	assert.Equal(t, mockResponse.DATA[0].Balance, response.DATA[0].Balance, "Неожиданное значение balance")
+	assert.Equal(t, mockResponse.DATA[0].Credit, response.DATA[0].Credit, "Неожиданное значение credit")
+	assert.Equal(t, mockResponse.DATA[0].Ordered, response.DATA[0].Ordered, "Неожиданное значение ordered")
+	assert.Equal(t, mockResponse.DATA[0].Reserved, response.DATA[0].Reserved, "Неожиданное значение reserved")
+	assert.Equal(t, mockResponse.DATA[0].Limit, response.DATA[0].Limit, "Неожиданное значение limit")
+	assert.Equal(t, mockResponse.DATA[0].PayTomorrow, response.DATA[0].PayTomorrow, "Неожиданное значение payTomorrow")
+	assert.Equal(t, mockResponse.DATA[0].ShippingFrom, response.DATA[0].ShippingFrom, "Неожиданное значение shippingForm")
+	assert.Equal(t, mockResponse.DATA[0].Active, response.DATA[0].Active, "Неожиданное значение active")
+}
