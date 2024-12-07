a630f3fa0ba30c9c29a1ab0b944678d1558c74ec
Vasiliy Rozhkov <profilrv@gmail.com>
2024-12-07 18:42:35 +0500

    test: SearchItems

--- test/service/service_test.go
+++ test/service/service_test.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"encoding/json"
+	"github.com/LX4777/autoeuro-go-api-client/client/requests"
 	"github.com/LX4777/autoeuro-go-api-client/client/responses"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -67,4 +68,123 @@ func TestGetBrands(t *testing.T) {
 	assert.Equal(t, 2, len(response.DATA), "длина массива не равна 2")
 	assert.Equal(t, "MANN-FILTER", response.DATA[0].Brand, "имя бренда не соответствует ожидаемому")
 	assert.Equal(t, "MANN", response.DATA[1].Brand, "имя бренда не соответствует ожидаемому")
+}
+
+func TestSearchItems(t *testing.T) {
+	jsonMock := []byte(`
+	{
+		"DATA": [
+			{
+				"offer_key": "2", 
+				"stock": 33, 
+				"cross": 2, 
+				"brand": "Brand2", 
+				"code": "Code2", 
+				"name": "Item 2", 
+				"packing": 2, 
+				"price": 240.3, 
+				"currency": "USD", 
+				"amount": 3, 
+				"unit": "кт", 
+				"return": 2, 
+				"order_before": "3", 
+				"delivery_time": "1211", 
+				"delivery_time_max": "2322", 
+				"rejects": 95.0, 
+				"dealer": 0, 
+				"warehouse_name": "WHN2", 
+				"warehouse_key": "WHK2"
+			},
+			{
+				"offer_key": "2", 
+				"stock": 33, 
+				"cross": "2", 
+				"brand": "Brand2", 
+				"code": "Code2", 
+				"name": "Item 2", 
+				"packing": "2", 
+				"price": "240.0", 
+				"currency": "USD", 
+				"amount": 3, 
+				"unit": "кт", 
+				"return": "2", 
+				"order_before": "3", 
+				"delivery_time": "1211", 
+				"delivery_time_max": "2322", 
+				"rejects": 95, 
+				"dealer": 0, 
+				"warehouse_name": "WHN2", 
+				"warehouse_key": "WHK2"
+			},
+			{
+				"offer_key": "2", 
+				"stock": 33, 
+				"cross": null, 
+				"brand": "Brand2", 
+				"code": "Code2", 
+				"name": "Item 2", 
+				"packing": "2", 
+				"price": "240", 
+				"currency": "USD", 
+				"amount": 3, 
+				"unit": "кт", 
+				"return": "2", 
+				"order_before": "3", 
+				"delivery_time": "1211", 
+				"delivery_time_max": "2322", 
+				"rejects": 95.0, 
+				"dealer": 0, 
+				"warehouse_name": "WHN2", 
+				"warehouse_key": "WHK2"
+			},
+			{
+				"offer_key": "2", 
+				"stock": 33, 
+				"brand": "Brand2", 
+				"code": "Code2", 
+				"name": "Item 2", 
+				"packing": "2", 
+				"price": 240, 
+				"currency": "USD", 
+				"amount": 3, 
+				"unit": "кт", 
+				"return": 2, 
+				"order_before": "3", 
+				"delivery_time": "1211", 
+				"delivery_time_max": "2322", 
+				"rejects": 95.0, 
+				"dealer": 0, 
+				"warehouse_name": "WHN2", 
+				"warehouse_key": "WHK2"
+			}
+		]
+	}`)
+
+	ts := NewTestServer("/search_items", jsonMock)
+	defer ts.Close()
+
+	response, err := ts.Service.SearchItems(requests.SearchItemsRequestData{})
+
+	assert.NoError(t, err, "Неожиданная ошибка от SearchItems")
+	assert.NotNil(t, response, "Ответ не получен")
+	assert.Equal(t, len(response.DATA), 4, "неожиданное количество элементов")
+	assert.IsTypef(t, responses.SearchItemsResponse{}, *response, "тип ответа не соответствует ожидаемому")
+	assert.Equal(t, "2", response.DATA[0].OfferKey, "неожиданное значение OfferKey")
+	assert.Equal(t, 33, response.DATA[0].Stock, "неожиданное значение Stock")
+	assert.Equal(t, "Brand2", response.DATA[0].Brand, "неожиданное значение Brand")
+	assert.Equal(t, "Code2", response.DATA[0].Code, "неожиданное значение Code")
+	assert.Equal(t, "Item 2", response.DATA[0].Name, "неожиданное значение Name")
+	assert.Equal(t, 2, response.DATA[0].Packing, "неожиданное значение Packing")
+	assert.Equal(t, float32(240.3), response.DATA[0].Price, "неожиданное значение Price")
+	assert.Equal(t, "USD", response.DATA[0].Currency, "неожиданное значение Currency")
+	assert.Equal(t, 3, response.DATA[0].Amount, "неожиданное значение Amount")
+	assert.Equal(t, "кт", response.DATA[0].Unit, "неожиданное значение Unit")
+	assert.Equal(t, uint8(2), response.DATA[0].Return, "неожиданное значение Return")
+	assert.Equal(t, "3", response.DATA[0].OrderBefore, "неожиданное значение OrderBefore")
+	assert.Equal(t, "1211", response.DATA[0].DeliveryTime, "неожиданное значение DeliveryTime")
+	assert.Equal(t, "2322", response.DATA[0].DeliveryTimeMax, "неожиданное значение DeliveryTimeMax")
+	assert.Equal(t, float32(95.0), response.DATA[0].Rejects, "неожиданное значение Rejects")
+	assert.Equal(t, 0, response.DATA[0].Dealer, "неожиданное значение Dealer")
+	assert.Equal(t, "WHN2", response.DATA[0].WarehouseName, "неожиданное значение WarehouseName")
+	assert.Equal(t, "WHK2", response.DATA[0].WarehouseKey, "неожиданное значение WarehouseKey")
 }
